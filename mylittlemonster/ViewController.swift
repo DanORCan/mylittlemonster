472db6a7f9c80a9a4e2dc4678cb53f09355e1a5d
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2015-12-22 02:33:18 -0500

    Initial Commit

---
+++ mylittlemonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mylittlemonster
+//
+//  Created by DANIEL OREILLY on 12/22/15.
+//  Copyright © 2015 DANIEL OREILLY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
