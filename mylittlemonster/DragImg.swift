37209bdb5b324551c5c64927641b0bdbb1905de3
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2015-12-25 11:22:12 -0500

    end of lecture 54

--- mylittlemonster/DragImg.swift
+++ mylittlemonster/DragImg.swift
@@ -10,6 +10,11 @@ import Foundation
 import UIKit
 
 class DragImg: UIImageView {
+    
+    var originalPosition:  CGPoint!
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -21,14 +26,19 @@ class DragImg: UIImageView {
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         //touch screen
+        originalPosition = self.center
     }
     
     override func   touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        //
+        if let touch = touches.first {
+            let position = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x, position.y)
+        }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        //
+        self.center = originalPosition
+        
     }
     
 }
